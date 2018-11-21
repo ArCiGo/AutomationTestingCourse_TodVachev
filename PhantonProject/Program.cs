a06b93b538cda9a67c8aede9ac6839e1464df639
ArCiGo <arcigo@Testing-ArCiGo-iTexico.local>
2018-11-21 14:32:59 -0600

    Add of PhantonProject (WIP)

---
+++ PhantonProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PhantonProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+
+            List<string> extractedLinks = new List<string>();
+            IWebDriver driver = new ChromeDriver(options);
+            string siteMapURL = @"http://testing.todvachev.com/sitemap-posttype-post.xml";
+            int startIndex = 0;
+            int linkLength = 0;
+
+            driver.Navigate().GoToUrl(siteMapURL);
+
+            string[] pageSource = driver.PageSource.Split(' ');
+
+            foreach (var item in pageSource)
+            {
+                if(item.Contains(@"href=""http://testing.todvachev.com/"))
+                {
+                    startIndex = item.IndexOf(@"href=\", StringComparison.CurrentCulture) + 7;
+                    linkLength = item.LastIndexOf(@""">", StringComparison.CurrentCulture) - startIndex;
+
+                    extractedLinks.Add(item.Substring(startIndex, linkLength));
+                }
+            }
+
+            // Console.WriteLine(driver.PageSource);
+        }
+    }
+}
